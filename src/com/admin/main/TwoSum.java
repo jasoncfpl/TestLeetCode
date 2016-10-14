2157fc69e29c140b5c1adcdf186f336314c6badb
jia <cnexcelee@gmail.com>
2016-10-14 11:03:38 +0800

    增加2个优化之后的方法

--- src/com/admin/main/TwoSum.java
+++ src/com/admin/main/TwoSum.java
@@ -34,4 +34,32 @@ public static int[] twoSum(int[] nums, int target) {
         return arr;
             
     }
+//beats 19.69%
+public static int[] twoSum1(int[] nums, int target) {
+      int[] arr = {-1,-1};
+      for(int i=0;i<nums.length;i++){
+          for(int j = i+1; j<nums.length;j++){
+          		int temp = nums[i] + nums[j];
+                  if( temp == target){
+                       arr[0] = i;
+                       arr[1] = j;
+                       return arr;
+                  }
+          }
+      }
+      return arr;
+          
+  }
+//29.32%
+public static int[] twoSum2(int[] nums, int target) {
+    for(int i=0;i<nums.length;i++){
+        for(int j = i+1; j<nums.length;j++){
+        		int temp = nums[i] + nums[j];
+                if( temp == target){
+                     return new int[]{i,j};
+                }
+        }
+    }
+	return null;
+}
 }
