17761d126ef6bbe202ecc85b751634a4d120f41a
jia <cnexcelee@gmail.com>
2016-10-14 10:54:27 +0800

    优化方法速度

--- src/com/admin/main/TwoSum.java
+++ src/com/admin/main/TwoSum.java
@@ -3,8 +3,8 @@ package com.admin.main;
 public class TwoSum {
 
 	public static void main(String[] args) {
-		int[] nums = {3,2,4,7,6,1,8,9};
-		int target = 12;
+		int[] nums = {3,2,4};
+		int target = 6;
 		long start = System.currentTimeMillis();
 		int[] arr = twoSum(nums,target);
 		long end = System.currentTimeMillis();
@@ -14,24 +14,18 @@ public class TwoSum {
 			System.out.print(arr[i]+",");
 		}
 	}
-	
+
+//beats 19.13%
 public static int[] twoSum(int[] nums, int target) {
-        
-        int firstValue,secValue;
         int[] arr = {-1,-1};
         for(int i=0;i<nums.length;i++){
-            firstValue = nums[i];
-            for(int j = 0; j<nums.length;j++){
-//            	if(i == j){
-//            		continue;
-//            	}
-                secValue = nums[j];
-                int temp = firstValue + secValue;
-                if( temp == target && i!= j){
-                     arr[0] = i;
-                     arr[1] = j;
-                     break;
-                }
+            for(int j = i+1; j<nums.length;j++){
+            		int temp = nums[i] + nums[j];
+                    if( temp == target){
+                         arr[0] = i;
+                         arr[1] = j;
+                         break;
+                    }
             }
             if(arr[0]!=-1 && arr[1] !=-1){
             	break;
